2144ea1cf4d82db523f909032dc9d145fae70931
Marco Sirabella <marco@sirabella.org>
2018-10-26 21:21:36 -0400

    Allow intent shared subject or title to be mms subject
    Test: m, manual
    Change-Id: Ibb5eba030320467774cdac1d502ec36c98abcb43

--- src/com/android/messaging/ui/conversationlist/ShareIntentActivity.java
+++ src/com/android/messaging/ui/conversationlist/ShareIntentActivity.java
@@ -81,6 +81,14 @@ public class ShareIntentActivity extends BaseBugleActivity implements
     public void onAttachFragment(final Fragment fragment) {
         final Intent intent = getIntent();
         final String action = intent.getAction();
+
+        String sharedSubject = null;
+        if (intent.hasExtra(Intent.EXTRA_SUBJECT)) {
+            sharedSubject = intent.getStringExtra(Intent.EXTRA_SUBJECT);
+        } else if (intent.hasExtra(Intent.EXTRA_TITLE)) {
+            sharedSubject = intent.getStringExtra(Intent.EXTRA_TITLE);
+        }
+
         if (Intent.ACTION_SEND.equals(action)) {
             final Uri contentUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
             if (UriUtil.isFileUri(contentUri)) {
@@ -101,11 +109,14 @@ public class ShareIntentActivity extends BaseBugleActivity implements
                     // Try to get text string from content uri.
                     sharedText = getTextStringFromContentUri(contentUri);
                 }
-                mDraftMessage =
-                        sharedText != null ? MessageData.createSharedMessage(sharedText) : null;
+                if (sharedText != null) {
+                    mDraftMessage = MessageData.createSharedMessage(sharedText, sharedSubject);
+                } else {
+                    mDraftMessage = null;
+                }
             } else if (PendingAttachmentData.isSupportedMediaType(contentType)) {
                 if (contentUri != null) {
-                    mDraftMessage = MessageData.createSharedMessage(null);
+                    mDraftMessage = MessageData.createSharedMessage(null, sharedSubject);
                     addSharedPartToDraft(contentType, contentUri);
                 } else {
                     mDraftMessage = null;
@@ -149,7 +160,8 @@ public class ShareIntentActivity extends BaseBugleActivity implements
                 }
 
                 if (strBuffer.length() > 0 || !uriMap.isEmpty()) {
-                    mDraftMessage = MessageData.createSharedMessage(strBuffer.toString());
+                    mDraftMessage =
+                            MessageData.createSharedMessage(strBuffer.toString(), sharedSubject);
                     for (final Map.Entry<Uri, String> e : uriMap.entrySet()) {
                         addSharedPartToDraft(e.getValue(), e.getKey());
                     }
